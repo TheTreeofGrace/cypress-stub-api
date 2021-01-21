7c0be6d55978f1b0f9b24bc69bd36ca8c546f867
GraceATree <grace.tree@ecs-digital.co.uk>
2021-01-21 15:34:04 +0000

    dynamic stubbing in cypress

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -4,18 +4,36 @@ describe('Feedback Form Tests', () => {
     const submitBtn = '[data-qa="submitBtn"]';
 
     it('should send API request and display Error component', () => {
-        cy.intercept('POST', '/your-backend-api').as('backendAPI');
+        let dynamicStatusCodeStub = 404;
+        cy.intercept('POST', '/your-backend-api', (req) => {
+            req.reply({
+                statusCode: dynamicStatusCodeStub, 
+            });
+        }).as('backendAPI');
+
         cy.visit(Cypress.env('HOST'));
+
         cy.get(submitBtn).should('be.visible');
         cy.get(submitBtn).click();
 
         cy.wait('@backendAPI').then(xhr => {
             expect(xhr.response.statusCode).to.equal(404);
+            dynamicStatusCodeStub = 200;
         });
         cy.get('h1').should('contain', 'Oops something went wrong!');
         cy.get('h1').should('not.contain', 'Feedback Form');
-    });
 
-    // TO DO: test the sucess path of this application by using cy.intercept! 
-    // Don't mock or touch the code!
+        cy.reload();
+        
+        cy.get(submitBtn).should('be.visible');
+        cy.get(submitBtn).click();
+
+        cy.wait('@backendAPI').then(xhr => {
+            expect(xhr.response.statusCode).to.equal(200);
+        });
+
+        cy.get('h1').should('contain', 'Thank you for your feedback!');
+        cy.get('h1').should('not.contain', 'Feedback Form');
+        cy.get('h1').should('not.contain', 'Oops something went wrong!');
+    });
 });
